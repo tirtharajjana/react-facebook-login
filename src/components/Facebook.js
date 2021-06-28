e7655f2e961273e797e7ee140412e3b2635a9ba5
Tirtharaj <tirtharajjana615@gmail.com>
2021-06-28 20:08:25 +0530

    first commit

---
+++ src/components/Facebook.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import FacebookLogin from 'react-facebook-login'
+
+export class Facebook extends Component {
+    state={
+        isLoggedIn:false,
+        userID:'',
+        name:'',
+        email:'',
+        picture:''
+    }
+
+    render() {
+        let fbContent;
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Facebook
